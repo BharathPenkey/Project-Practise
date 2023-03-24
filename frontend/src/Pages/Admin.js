da105bcebe0be0a9e1178831eec736384fb410ff
parul-jaiswal2002 <paruljansie@gmail.com>
2023-03-24 15:07:07 +0530

    added some kind of folders and some files

--- frontend/src/Pages/Admin.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Admin(){
-    return <>
-    admin page
-    </>
-}
- export default Admin;
--- frontend/src/Pages/carbooking.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const carbooking = () => {
-  return (
-    <div>carbooking</div>
-  )
-}
-
-export default carbooking
